6251924ee74c24ca7094bb0de2791c25e5ae6fd5
4itech-paul <paul@4idps.com>
2023-10-24 16:10:40 +0800

    chore(env): add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        schema: configService.get('DB_SCHEMA'),
+        autoLoadEntities: true, // every entity registered through the forFeature() method will be automatically added to the entities array of the configuration object.
+        synchronize: true, // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
+        logging: !!configService.get('DB_LOGGING'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
